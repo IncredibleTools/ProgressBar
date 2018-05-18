3a2c0557201d17e627d1c1ed04ef2e91d3581804
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2018-05-18 11:12:34 -0300

    Update ProgressBar.cpp

--- Windows/ProgressBar.cpp
+++ Windows/ProgressBar.cpp
@@ -17,29 +17,24 @@ void ProgressBar::showProgressBar(uint32_t ui32_increase){
   showConsoleCursor(false);
   uint32_t ui32_newIncrease = ui32_roundValue((float)ui32_increase*(float)this->f_maxConstant);
   bool b_flagPrintPercentageProgress = false;
+  this->f_percentageValue = (float)ui32_increase*(float)this->f_maxConstant/(float)this->ui32_maxBar*(float)100;
   if ((float)ui32_increase*(float)this->f_maxConstant > ui32_barValue){
-    b_flagPrintPercentageProgress = true;
+    this->ui32_barValue = ui32_newIncrease;
   }
-  if(this->ui32_maxBar >= ui32_newIncrease){
-    printf ("\r");
-    printf("%c", this->c_frame);
-    for(uint32_t ui32_counter = 0; ui32_counter < ui32_newIncrease; ui32_counter++){
-      printf("%c", this->c_progressBar);
-    }
-    if (b_flagPrintPercentageProgress == true){
-      this->ui32_barValue = ui32_newIncrease;
-      this->f_percentageValue = (float)ui32_increase*(float)this->f_maxConstant/(float)this->ui32_maxBar*(float)100;
-    }
-    while(ui32_newIncrease < this->ui32_maxBar){
-      printf("%c", this->c_shadowBar);
-      ui32_newIncrease++;
-    }
-    if (f_percentageValue == 100){
-      showConsoleCursor(true);
-    }
-    printf("%c", this->c_frame);
-    printf(" - %.2f%c", (float)this->f_percentageValue, 37);
-  }
+	printf ("\r");
+	printf("%c", this->c_frame);
+	for(uint32_t ui32_counter = 0; ui32_counter < ui32_newIncrease; ui32_counter++){
+	  printf("%c", this->c_progressBar);
+	}
+	while(ui32_newIncrease < this->ui32_maxBar){
+	  printf("%c", this->c_shadowBar);
+	  ui32_newIncrease++;
+	}
+	if (f_percentageValue == 100){
+	  showConsoleCursor(true);
+	}
+	printf("%c", this->c_frame);
+	printf(" - %.2f%c", (float)this->f_percentageValue, 37);
 }
 
 uint32_t ProgressBar::ui32_roundValue(float f_value){
